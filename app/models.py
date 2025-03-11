417f455cc9c8c15d3e4a8e92377f3552612ad828
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2025-03-11 14:04:24 -0400

    create db model for category and community post

--- app/models.py
+++ app/models.py
@@ -5,3 +5,25 @@ class User(BaseModel):
     name: str
     email: str
     age: Optional[int] = None
+
+
+class communityPost(BaseModel):
+    id: int
+    title: str
+    content: str
+    author: str
+    date: str
+
+
+
+class categoryPost(BaseModel):
+    id: int
+    title: str
+    category: str
+    content: str
+    descritpion: str
+    author: str
+    timestamp: str
+
+
+
