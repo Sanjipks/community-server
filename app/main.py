68dbac5d07457584c162dfa7d9d1a729cb254d48
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2025-07-19 12:32:29 -0400

    refactor: reorganize code and place in separate file for chatlist

--- app/main.py
+++ app/main.py
@@ -1,16 +1,15 @@
-from datetime import datetime
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from app.database import get_database
-from app.models import postCommunityPost, postJoke, user
 from app.routes.community_posts import router as community_posts_router
 from app.routes.categories import router as categories_router
 from app.routes.fun import router as fun_router
 from app.routes.createUser import router as createUser_router
 from app.routes.userlist import router as userlist_router
+from app.routes.chatlist import router as chatlist_router
 from dotenv import load_dotenv
-from bson import ObjectId
+
 load_dotenv()
 import os
 
@@ -33,11 +32,13 @@ origins = [
 ]
 
 print("Routers linked successfully!")
+
 app.include_router(community_posts_router, prefix="/community", tags=["Community Posts"])
 app.include_router(categories_router, prefix="/categories", tags=["Categories"])
 app.include_router(fun_router, prefix="/fun", tags=["fun"])
 app.include_router(createUser_router, prefix="/create-user", tags=["Create User"])
 app.include_router(userlist_router, prefix="/users", tags=["Userlist"])
+app.include_router(chatlist_router, prefix="/chatlist", tags=["Chatlist"])
 
 app.add_middleware(
     CORSMiddleware,
@@ -53,45 +54,6 @@ async def root():
 
 
 
-@app.get("/users")
-async def get_users():
-    users = await db["users"].find().to_list(length=100)
-    for user in users:
-        user["id"] = str(user["_id"])
-        del user["_id"]
-   
-    return users
 
-@app.post("/chatlist/adduser") 
-async def add_user(user: postCommunityPost):
-    user_dict = user.model_dump()
-    result = await db["user"].insert_one(user_dict)
-    if result.inserted_id:
-        return {"message": "User added successfully"}
-    else:
-        raise HTTPException(status_code=500, detail="Failed to add user")
-    
-@app.get("/chatlist/getusers")
-async def get_users_list():
-    users = await db["user"].find().to_list(length=100)
-    for user in users:
-        user["id"] = str(user["_id"])
-        del user["_id"]
-    return users
 
-@app.delete("/chatlist/removeuser") 
-async def remove_user(request: user):
-    try:
-        print('Received ID:', request.id)
-        # Convert the string id to ObjectId
-        object_id = ObjectId(request.id)
-        print('ObjectId:', object_id)
-    except Exception as e:
-        print("Error occurred:", str(e))
-        raise HTTPException(status_code=400, detail="Invalid user ID format")
 
-    result = await db["user"].delete_one({"_id": object_id})
-    if result.deleted_count == 1:
-        return {"message": "User deleted successfully"}
-    else:
-        raise HTTPException(status_code=404, detail="User not found")
---
+++ app/routes/chatlist.py
@@ -0,0 +1,45 @@
+from bson import ObjectId
+from app.database import get_database
+from fastapi import APIRouter, HTTPException
+
+from app.models import postCommunityPost, user
+
+
+router = APIRouter()
+@router.post("/chatlist/adduser") 
+async def add_user(user: postCommunityPost):
+    db = await get_database()
+    user_dict = user.model_dump()
+    result = await db["user"].insert_one(user_dict)
+    if result.inserted_id:
+        return {"message": "User added successfully"}
+    else:
+        raise HTTPException(status_code=500, detail="Failed to add user")
+    
+@router.get("/chatlist/getusers")
+async def get_users_list():
+    db = await get_database()
+    users = await db["user"].find().to_list(length=100)
+    for user in users:
+        user["id"] = str(user["_id"])
+        del user["_id"]
+    return users
+
+
+@router.delete("/chatlist/removeuser") 
+async def remove_user(request: user):
+    db = await get_database()
+    try:
+        print('Received ID:', request.id)
+        # Convert the string id to ObjectId
+        object_id = ObjectId(request.id)
+        print('ObjectId:', object_id)
+    except Exception as e:
+        print("Error occurred:", str(e))
+        raise HTTPException(status_code=400, detail="Invalid user ID format")
+
+    result = await db["user"].delete_one({"_id": object_id})
+    if result.deleted_count == 1:
+        return {"message": "User deleted successfully"}
+    else:
+        raise HTTPException(status_code=404, detail="User not found")
