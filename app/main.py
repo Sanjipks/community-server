8f7b2fdd9bc2149fd7657b776a81e3808b52a7f7
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2025-08-30 21:59:34 -0400

    add route to main page for user login

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ from app.routes.community_posts import router as community_posts_router
 from app.routes.categories import router as categories_router
 from app.routes.fun import router as fun_router
 from app.routes.createUser import router as createUser_router
+from app.routes.userLogin import router as userLogin_router
 from app.routes.userlist import router as userlist_router
 from app.routes.chatlist import router as chatlist_router
 from dotenv import load_dotenv
@@ -37,6 +38,7 @@ app.include_router(community_posts_router, prefix="/community", tags=["Community
 app.include_router(categories_router, prefix="/categories", tags=["Categories"])
 app.include_router(fun_router, prefix="/fun", tags=["fun"])
 app.include_router(createUser_router, prefix="/create-user", tags=["Create User"])
+app.include_router(userLogin_router, prefix="/login-user", tags=["User Lgoin"])
 app.include_router(userlist_router, prefix="/users", tags=["Userlist"])
 app.include_router(chatlist_router, prefix="/chatlist", tags=["Chatlist"])
 
