18ab9bd9a1ac5d282db906a70a20d5d6a6e58bd4
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2025-10-20 03:01:26 -0400

    ..

--- app/main.py
+++ app/main.py
@@ -45,7 +45,6 @@ app.include_router(fun_router, prefix="/fun", tags=["fun"])
 app.include_router(createUser_router, prefix="/create-user", tags=["Create User"])
 app.include_router(userLogin_router, prefix="/login-user", tags=["User Lgoin"])
 app.include_router(userlist_router, prefix="/users", tags=["User Lists"])
-# app.include_router(deleteUser_router, prefix="/users", tags=["Delete User"])
 app.include_router(chatlist_router, prefix="/chatlist", tags=["Chatlist"])
 app.include_router(forgotpassword_router, prefix="/forgotpassword", tags=["Forgot Password"])
 
